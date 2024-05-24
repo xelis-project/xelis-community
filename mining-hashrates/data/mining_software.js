ecb7a887e24f270771333e07be1bea14051dd328
g45t345rt <49697006+g45t345rt@users.noreply.github.com>
2024-05-24 10:45:24 -0400

    add mining softwares

---
+++ mining-hashrates/data/mining_software.js
@@ -0,0 +1,10 @@
+const MINING_SOFTWARE = [
+  "Official Miner v1.11.0",
+  "XELIS GPU MINER (allfather) v0.9.2",
+  "XELIS Miner (DeroLuna) v0.3",
+  "TNN Miner v0.3.5-beta",
+  "Rigel v1.17.2",
+  "SRBMiner-MULTI v2.5.4",
+  "OneZeroMiner v1.3.3",
+  "ProfitMiner (fatigue24) v2.2",
+]
