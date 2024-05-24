772951a5608aaa231392a5f35e4cf0c34fd23e12
g45t345rt <49697006+g45t345rt@users.noreply.github.com>
2024-05-24 11:35:35 -0400

    add more cpus epyc, threadripper, xeon

--- mining-hashrates/data/intel_cpu.js
+++ mining-hashrates/data/intel_cpu.js
@@ -1,58 +1,128 @@
 const INTEL_CPU = [
-  // 10th Generation "Comet Lake" (2020)
-  "Core i9-10900K",
-  "Core i7-10700K",
-  "Core i5-10600K",
-  "Core i3-10320",
-  "Core i3-10100",
-  "Core i9-10980XE",
+  // 13th Generation "Raptor Lake" (2022-2023)
+  "Core i9-13900K",
+  "Core i9-13900KS",
+  "Core i7-13700K",
+  "Core i7-13700",
+  "Core i5-13600K",
+  "Core i5-13500",
+  "Core i5-13400",
+  "Core i3-13100",
+
+  // 12th Generation "Alder Lake" (2021-2022)
+  "Core i9-12900K",
+  "Core i9-12900KS",
+  "Core i7-12700K",
+  "Core i7-12700",
+  "Core i5-12600K",
+  "Core i5-12600",
+  "Core i5-12500",
+  "Core i5-12400",
+  "Core i3-12300",
+  "Core i3-12100",
 
   // 11th Generation "Rocket Lake" (2021)
   "Core i9-11900K",
   "Core i7-11700K",
+  "Core i7-11700",
   "Core i5-11600K",
+  "Core i5-11600",
+  "Core i5-11500",
   "Core i5-11400",
+  "Core i3-11300",
   "Core i3-11100",
 
-  // 12th Generation "Alder Lake" (2021)
-  "Core i9-12900K",
-  "Core i7-12700K",
-  "Core i5-12600K",
-  "Core i5-12400",
-  "Core i3-12100",
+  // 10th Generation "Comet Lake" (2020)
+  "Core i9-10900K",
+  "Core i9-10900",
+  "Core i7-10700K",
+  "Core i7-10700",
+  "Core i5-10600K",
+  "Core i5-10600",
+  "Core i5-10500",
+  "Core i5-10400",
+  "Core i3-10320",
+  "Core i3-10300",
+  "Core i3-10100",
+  "Core i9-10980XE",
+  "Core i9-10940X",
+  "Core i9-10920X",
+  "Core i9-10900X",
 
-  // 13th Generation "Raptor Lake" (2022)
-  "Core i9-13900K",
-  "Core i7-13700K",
-  "Core i5-13600K",
-  "Core i5-13400",
-  "Core i3-13100",
-
-  // 9th Generation "Coffee Lake Refresh" (2018)
+  // 9th Generation "Coffee Lake Refresh" (2018-2019)
   "Core i9-9900K",
+  "Core i9-9900KS",
+  "Core i9-9900",
   "Core i7-9700K",
+  "Core i7-9700",
   "Core i5-9600K",
+  "Core i5-9600KF",
+  "Core i5-9600",
+  "Core i5-9500",
+  "Core i5-9400",
   "Core i5-9400F",
+  "Core i3-9350K",
+  "Core i3-9320",
+  "Core i3-9300",
   "Core i3-9100",
 
-  // 8th Generation "Coffee Lake" (2017)
+  // 8th Generation "Coffee Lake" (2017-2018)
   "Core i7-8700K",
+  "Core i7-8700",
   "Core i5-8600K",
+  "Core i5-8600",
+  "Core i5-8500",
   "Core i5-8400",
   "Core i3-8350K",
+  "Core i3-8300",
   "Core i3-8100",
 
-  // 7th Generation "Kaby Lake" (2016)
-  "Core i7-7700K",
-  "Core i5-7600K",
-  "Core i5-7500",
-  "Core i3-7350K",
-  "Core i3-7100",
+  // Xeon Scalable Processors (2018-2023)
+  // 4th Gen Xeon "Sapphire Rapids" (2023)
+  "Xeon Platinum 8490H",
+  "Xeon Platinum 8480+",
+  "Xeon Platinum 8480H",
+  "Xeon Platinum 8472C",
+  "Xeon Platinum 8470N",
+  "Xeon Platinum 8468",
+  "Xeon Gold 6454R",
+  "Xeon Gold 6450",
+  "Xeon Gold 6438N",
+  "Xeon Silver 4416",
+  "Xeon Silver 4414",
 
-  // 6th Generation "Skylake" (2015-2016)
-  "Core i7-6700K",
-  "Core i5-6600K",
-  "Core i5-6500",
-  "Core i3-6300",
-  "Core i3-6100"
+  // 3rd Gen Xeon "Ice Lake-SP" (2021)
+  "Xeon Platinum 8380",
+  "Xeon Platinum 8376H",
+  "Xeon Platinum 8368Q",
+  "Xeon Platinum 8358P",
+  "Xeon Platinum 8358",
+  "Xeon Gold 6348",
+  "Xeon Gold 6338N",
+  "Xeon Gold 6326",
+  "Xeon Gold 5317",
+  "Xeon Silver 4316",
+  "Xeon Silver 4309Y",
+
+  // 2nd Gen Xeon "Cascade Lake" (2019)
+  "Xeon Platinum 8280",
+  "Xeon Platinum 8276L",
+  "Xeon Platinum 8268",
+  "Xeon Gold 6248R",
+  "Xeon Gold 6246R",
+  "Xeon Gold 6238T",
+  "Xeon Gold 6238R",
+  "Xeon Gold 5222",
+  "Xeon Silver 4216",
+  "Xeon Silver 4214",
+
+  // 1st Gen Xeon "Skylake-SP" (2017-2018)
+  "Xeon Platinum 8180",
+  "Xeon Platinum 8176",
+  "Xeon Platinum 8170",
+  "Xeon Gold 6138",
+  "Xeon Gold 6132",
+  "Xeon Gold 5122",
+  "Xeon Silver 4116",
+  "Xeon Silver 4114"
 ]
